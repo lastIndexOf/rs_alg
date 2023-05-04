0f8400d6197154c2f5a6f0fa945b4ec891383fca
zhengfankai <zhengfankai@bytedance.com>
2023-05-04 11:56:50 +0800

    chore: leet code 14

--- src/leetcode/fourteen.rs
+++ src/leetcode/fourteen.rs
@@ -1,7 +1,31 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn longest_common_prefix(strs: Vec<String>) -> String {}
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        let mut chars = strs.iter().map(|s| s.as_str().chars()).collect::<Vec<_>>();
+
+        let mut res = String::from("");
+        let mut common: char;
+
+        loop {
+            let next = chars[0].next();
+
+            if next.is_none() {
+                return res;
+            } else {
+                common = next.unwrap();
+            }
+            for idx in 1..chars.len() {
+                let c = &mut chars[idx];
+                match c.next() {
+                    Some(next_char) if next_char == common => {}
+                    _ => return res,
+                }
+            }
+
+            res.push_str(&format!("{common}"));
+        }
+    }
 }
 
 #[cfg(test)]
