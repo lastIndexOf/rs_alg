4f1b2f3d8914ad1058fd60a2a5e76c0ab301c84f
zhengfankai <zhengfankai@bytedance.com>
2023-05-09 01:20:35 +0800

    chore: leet code 19 with raw pointer better

--- src/leetcode/nineteen.rs
+++ src/leetcode/nineteen.rs
@@ -17,39 +17,82 @@ impl ListNode {
 pub struct Solution;
 
 impl Solution {
+    // pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
+    //     let mut dummy_head = Box::new(ListNode::new(0));
+    //     dummy_head.next = head;
+    //     let mut fast = &dummy_head.clone();
+    //     let mut slow = &mut dummy_head;
+    //     for i in 1..=n {
+    //         fast = fast.next.as_ref().unwrap();
+    //     }
+    //     while fast.next.is_some() {
+    //         fast = fast.next.as_ref().unwrap();
+    //         slow = slow.next.as_mut().unwrap();
+    //     }
+    //     slow.next = slow.next.as_mut().unwrap().next.take();
+    //     return dummy_head.next;
+    // }
+
     pub fn remove_nth_from_end(head: &mut Box<ListNode>, n: i32) -> Option<&Box<ListNode>> {
-        let mut idx = 0;
-        let mut node = &mut head.clone();
-        let mut parent = None;
+        let mut faster = &mut **head as *mut ListNode;
+        let mut slower = &mut **head as *mut ListNode;
 
-        while let Some(node_box) = node.next.as_mut() {
-            node = node_box;
-            idx += 1;
-
-            if idx == n {
-                if parent.is_none() {
-                    parent = Some(head.clone());
-                } else {
-                    parent = parent.unwrap().next;
+        unsafe {
+            for _ in 0..n {
+                if (*faster).next.is_none() {
+                    return head.next.as_ref();
                 }
+                faster = &mut **((*faster).next.as_mut().unwrap()) as *mut ListNode;
             }
+
+            while !(*faster).next.is_none() {
+                faster = &mut **((*faster).next.as_mut().unwrap()) as *mut ListNode;
+                slower = &mut **((*slower).next.as_mut().unwrap()) as *mut ListNode;
+            }
+
+            (*slower).next = (*slower).next.as_ref().unwrap().next.clone();
+
+            Some(head)
         }
-
-        if parent.is_none() {
-            return head.next.as_ref();
-        }
-
-        let cloned = parent.as_ref().unwrap().next.as_ref().unwrap().next.clone();
-        parent.unwrap().next = cloned;
-
-        Some(head)
     }
 }
 
 #[cfg(test)]
 mod nineteen_test {
-    // use super::*;
+    use crate::data_struct::linked_list;
+
+    use super::*;
 
     #[test]
-    fn test_nineteen() {}
+    fn test_nineteen() {
+        let mut linked_list = ListNode::new(1);
+        linked_list.next = Some(Box::new(ListNode::new(2)));
+        linked_list.next.as_mut().unwrap().next = Some(Box::new(ListNode::new(3)));
+        linked_list
+            .next
+            .as_mut()
+            .unwrap()
+            .next
+            .as_mut()
+            .unwrap()
+            .next = Some(Box::new(ListNode::new(4)));
+        linked_list
+            .next
+            .as_mut()
+            .unwrap()
+            .next
+            .as_mut()
+            .unwrap()
+            .next
+            .as_mut()
+            .unwrap()
+            .next = Some(Box::new(ListNode::new(5)));
+
+        let mut linked_list = &mut Box::new(linked_list);
+
+        assert_eq!(format!("{:?}", Solution::remove_nth_from_end(&mut linked_list, 3)), "Some(ListNode { val: 1, next: Some(ListNode { val: 2, next: Some(ListNode { val: 4, next: Some(ListNode { val: 5, next: None }) }) }) })");
+
+        println!("linked_list = {linked_list:?}");
+        // assert_eq!(format!("{:?}", Solution::remove_nth_from_end(&mut linked_list, 1)), "Some(ListNode { val: 2, next: Some(ListNode { val: 3, next: Some(ListNode { val: 4, next: None }) }) })");
+    }
 }
