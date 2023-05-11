9d4ae2461bb93f9ed18c0d57b544b9d7c8499e04
cold_sharp <kgaikj2cu@icloud.com>
2023-05-11 22:43:29 +0800

    chore: leet code 22

--- src/leetcode.rs
+++ src/leetcode.rs
@@ -9,3 +9,4 @@ pub mod ten;
 pub mod twelve;
 pub mod twenty;
 pub mod twenty_one;
+pub mod twenty_two;
---
+++ src/leetcode/twenty_two.rs
@@ -0,0 +1,60 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn generate_parenthesis(n: i32) -> Vec<String> {
+        let mut res = vec![];
+
+        do_generate_parenthesis(&mut res, &mut vec![], &mut String::new(), 0, 2 * n);
+
+        res
+    }
+}
+
+fn do_generate_parenthesis(
+    res: &mut Vec<String>,
+    stack: &mut Vec<char>,
+    current_str: &mut String,
+    start: i32,
+    n: i32,
+) {
+    if start == n {
+        if stack.is_empty() {
+            res.push(current_str.clone());
+        }
+        return;
+    }
+
+    stack.push('(');
+    current_str.push('(');
+    do_generate_parenthesis(res, stack, current_str, start + 1, n);
+    current_str.pop();
+    stack.pop();
+
+    if !stack.is_empty() && stack.last().unwrap() == &'(' {
+        current_str.push(')');
+        stack.pop();
+        do_generate_parenthesis(res, stack, current_str, start + 1, n);
+        stack.push('(');
+        current_str.pop();
+    }
+}
+
+#[cfg(test)]
+mod twenty_two_test {
+    use super::*;
+
+    #[test]
+    fn test_twenty_two() {
+        assert_eq!(Solution::generate_parenthesis(1), vec![String::from("()"),]);
+        assert_eq!(
+            Solution::generate_parenthesis(3),
+            vec![
+                String::from("((()))"),
+                String::from("(()())"),
+                String::from("(())()"),
+                String::from("()(())"),
+                String::from("()()()")
+            ]
+        );
+    }
+}
