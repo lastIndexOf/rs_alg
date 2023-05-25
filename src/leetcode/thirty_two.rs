f77858ca9df9e54d50f9e24525fdcceec0b4e554
cold_sharp <kgaikj2cu@icloud.com>
2023-05-25 22:26:19 +0800

    chore: leet code 32

--- src/leetcode/thirty_two.rs
+++ src/leetcode/thirty_two.rs
@@ -6,9 +6,8 @@ impl Solution {
         let len = s.len();
         let mut res = 0;
         let mut start = 0;
-        let mut stack = vec![];
-
         let mut idx = 0;
+        let mut stack = vec![];
 
         while idx < len {
             if s[idx] == '(' {
@@ -16,16 +15,15 @@ impl Solution {
                 idx += 1;
             } else {
                 match stack.pop() {
-                    Some(end_idx) => {
+                    Some(_) => {
                         if stack.is_empty() {
                             res = std::cmp::max(res, idx - start + 1);
                         } else {
-                            res = std::cmp::max(res, idx - end_idx + 1);
+                            res = std::cmp::max(res, idx - stack.last().unwrap());
                         }
                         idx += 1;
                     }
                     None => {
-                        res = std::cmp::max(res, idx - start);
                         idx += 1;
                         start = idx;
                     }
@@ -50,5 +48,10 @@ mod thirty_two_test {
         assert_eq!(Solution::longest_valid_parentheses("".to_string()), 0);
         assert_eq!(Solution::longest_valid_parentheses("()(())".to_string()), 6);
         assert_eq!(Solution::longest_valid_parentheses("()(()".to_string()), 2);
+        assert_eq!(
+            Solution::longest_valid_parentheses(")(((((()())()()))()(()))(".to_string()),
+            22
+        );
+        assert_eq!(Solution::longest_valid_parentheses("(()()".to_string()), 4);
     }
 }
