f698529c207f382c825ba170e7c4375b043d30d0
cold_sharp <kgaikj2cu@icloud.com>
2023-05-27 03:52:18 +0800

    chore: leet code 35(binarary search)

--- src/leetcode/thirty_five.rs
+++ src/leetcode/thirty_five.rs
@@ -8,10 +8,40 @@ impl Solution {
     //     }
     // }
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-        match nums.binary_search(&target) {
-            Ok(res) => return res as i32,
-            Err(res) => return res as i32,
+        if nums.len() == 1 {
+            match target.cmp(&nums[0]) {
+                std::cmp::Ordering::Greater => return 1,
+                _ => return 0,
+            }
         }
+
+        let mut left = 0;
+        let mut right = nums.len() - 1;
+        let mut middle = right / 2;
+
+        while left != right - 1 {
+            match nums[middle].cmp(&target) {
+                std::cmp::Ordering::Equal => return middle as i32,
+                std::cmp::Ordering::Greater => {
+                    right = middle;
+                    middle = (right - left) / 2 + left;
+                }
+                std::cmp::Ordering::Less => {
+                    left = middle;
+                    middle = (right - left) / 2 + left;
+                }
+            }
+        }
+
+        if target <= nums[left] {
+            return left as i32;
+        }
+
+        if target > nums[right] {
+            return (right + 1) as i32;
+        }
+
+        right as i32
     }
 }
 
@@ -26,5 +56,7 @@ mod thirty_five_test {
         assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 5), 2);
         assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 2), 1);
         assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 7), 4);
+        assert_eq!(Solution::search_insert(vec![1], 0), 0);
+        assert_eq!(Solution::search_insert(vec![1, 3], 1), 0);
     }
 }
