e6c85914d93219e6cf93afef64a52a52cfa8a264
cold_sharp <kgaikj2cu@icloud.com>
2023-05-28 16:48:08 +0800

    chore: leet code 36(Brute)

--- src/leetcode/thirty_six.rs
+++ src/leetcode/thirty_six.rs
@@ -2,7 +2,44 @@ pub struct Solution;
 
 impl Solution {
     pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
-        todo!()
+        for (x, x_value) in board.iter().enumerate() {
+            for (y, value) in x_value.into_iter().enumerate() {
+                if value != &'.' && !Self::valid_rule(&board, (x, y), *value) {
+                    return false;
+                }
+            }
+        }
+
+        true
+    }
+
+    fn valid_rule(board: &Vec<Vec<char>>, (x, y): (usize, usize), value: char) -> bool {
+        for i in 0..9 {
+            if board[x][i] == value && i != y {
+                return false;
+            }
+        }
+
+        for j in 0..9 {
+            if board[j][y] == value && j != x {
+                return false;
+            }
+        }
+
+        // nine grid
+
+        let x_start = x / 3 * 3;
+        let y_start = y / 3 * 3;
+
+        for i in x_start..(x_start + 3) {
+            for j in y_start..(y_start + 3) {
+                if board[i][j] == value && i != x && y != j {
+                    return false;
+                }
+            }
+        }
+
+        true
     }
 }
 
