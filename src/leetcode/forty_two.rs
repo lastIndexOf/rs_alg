d75bffdc9fef577b381ea44513cad46baf08ec49
cold_sharp <kgaikj2cu@icloud.com>
2023-05-31 21:32:41 +0800

    chore: leet code 42 init

---
+++ src/leetcode/forty_two.rs
@@ -0,0 +1,17 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn trap(height: Vec<i32>) -> i32 {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod forty_two_test {
+    use super::*;
+
+    #[test]
+    fn test_forty_two() {
+        // assert_eq!(Solution::first_missing_positive(vec![1, 2, 0]), 3);
+    }
+}
